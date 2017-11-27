24401b632c9e8e4de4205b6ca9b05d79e6f8f662
jpoell <j.poell@zeus.vumc.nl>
2017-11-27 10:52:49 +0100

    The end is coming\!

--- ACE.R
+++ ACE.R
@@ -1,5 +1,6 @@
 ##### ACE has arrived! #####
-# There are several functions: ACE, ploidyplotloop, ObjectsampleToTemplate, singlemodel, singleplot, getadjustedsegments, linkmutationdata, postanalysisloop, analyzegenomiclocations.
+# There are a number of functions: ACE, ploidyplotloop, ObjectsampleToTemplate, singlemodel, squaremodel, singleplot, 
+# getadjustedsegments, linkmutationdata, postanalysisloop, analyzegenomiclocations.
 # I have also added the "multiplot" function for making summary sheets.
 
 # ACE takes a folder with rds-files (default, make sure all are segmented) or bam-files and returns plots for the most likely errors in convenient subfolders
@@ -33,6 +34,11 @@
 # not strictly necessary to save it to a variable, but that might still be handy
 # singlemodel comes with the awesomeness of manual input: you can restrain the model to the ploidy you expect (default 2, but hey, ploidy 5 happens, right?)
 # and you can tell the program if you think there is a different standard (this should only be necessary when the median segment happens to be a subclonal variant; very rare)
+
+# squaremodel: this function drops the assumption of a standard, and instead runs the algorithm for each ploidy being the standard.
+# if you feel you are doing too much tinkering with variables using the singlemodel function, then try this!
+# you can choose the range of ploidies it should test; the default being all ploidies between 5 and 1 in 100 decrements of 0.04
+# like singlemodel, the function returns a list, which you can save to a variable
 
 # singleplot: again, you already know what it does! But did you know you can change the chart title to anything you like? Well, I'm here to tell you it can
 # and there is more! Don't believe the cellularities the program calculated for you? You don't have to! Just fill in the cellularity you believe to be true, and singleplot will take your word for it!
@@ -141,11 +147,11 @@ ploidyplotloop <- function(copyNumbersSegmented,currentdir,ploidies=2,imagetype=
   			expected[p] <- baseline + p*fraction[i-4]*standard/q
   			
   		  }
-  		  # the maximum error (e.g. 0.25 for RMSE) was added to make sure hyperamplifications (p>12) don't get ridiculous errors
+  		  # the maximum error 0.5 was added to make sure hyperamplifications (p>12) don't get ridiculous errors
   		  for (j in 1:length(segmentdata$values)) {
-  		    if(method=='RMSE') {temp[j] <- min(((segmentdata$values[j]-expected)/(fraction[i-4]^penalty))^2,0.25)}
-  		    else if(method=='SMRE') {temp[j] <- min(sqrt(abs((segmentdata$values[j]-expected)/(fraction[i-4]^penalty))),sqrt(0.5))}
-  		    else if(method=='MAE') {temp[j] <- min(abs((segmentdata$values[j]-expected)/(fraction[i-4]^penalty)),0.5)}
+  		    if(method=='RMSE') {temp[j] <- (min(abs(segmentdata$values[j]-expected),0.5)/(fraction[i-4]^penalty))^2}
+  		    else if(method=='SMRE') {temp[j] <- sqrt(min(abs(segmentdata$values[j]-expected),0.5)/(fraction[i-4]^penalty))}
+  		    else if(method=='MAE') {temp[j] <- min(abs(segmentdata$values[j]-expected),0.5)/(fraction[i-4]^penalty)}
   		    else {print("Not a valid method")}
   		  }
   		  if(method=='RMSE') {errorlist[i-4] <- sqrt(sum(rep(temp,segmentdata$lengths))/sum(segmentdata$lengths))}
@@ -391,9 +397,9 @@ singlemodel <- function(template,ploidy = 2, standard, QDNAseqobjectsample = FAL
 		  expected[p] <- baseline + p*fraction[i-4]*standard/ploidy
 		}
 		for (j in 1:length(segmentdata$values)) {
-		  if(method=='RMSE') {temp[j] <- min(((segmentdata$values[j]-expected)/(fraction[i-4]^penalty))^2,0.25)}
-		  else if(method=='SMRE') {temp[j] <- min(sqrt(abs((segmentdata$values[j]-expected)/(fraction[i-4]^penalty))),sqrt(0.5))}
-		  else if(method=='MAE') {temp[j] <- min(abs((segmentdata$values[j]-expected)/(fraction[i-4]^penalty)),0.5)}
+		  if(method=='RMSE') {temp[j] <- (min(abs(segmentdata$values[j]-expected),0.5)/(fraction[i-4]^penalty))^2}
+		  else if(method=='SMRE') {temp[j] <- sqrt(min(abs(segmentdata$values[j]-expected),0.5)/(fraction[i-4]^penalty))}
+		  else if(method=='MAE') {temp[j] <- min(abs(segmentdata$values[j]-expected),0.5)/(fraction[i-4]^penalty)}
 		  else {print("Not a valid method")}
 		}
 		if(method=='RMSE') {errorlist[i-4] <- sqrt(sum(rep(temp,segmentdata$lengths))/sum(segmentdata$lengths))}
@@ -490,9 +496,9 @@ squaremodel <- function(template, QDNAseqobjectsample = FALSE, prows=100, ptop=5
         expected[p] <- baseline + p*fraction[i-4]/ploidy
       }
       for (j in 1:length(segmentdata$values)) {
-        if(method=='RMSE') {temp[j] <- min(((segmentdata$values[j]-expected)*(1+abs(ploidy-2))^penploidy/(fraction[i-4]^penalty))^2,0.25)}
-        else if(method=='SMRE') {temp[j] <- min(sqrt(abs((segmentdata$values[j]-expected)*(1+abs(ploidy-2))^penploidy/(fraction[i-4]^penalty))),sqrt(0.5))}
-        else if(method=='MAE') {temp[j] <- min(abs((segmentdata$values[j]-expected)*(1+abs(ploidy-2))^penploidy/(fraction[i-4]^penalty)),0.5)}
+        if(method=='RMSE') {temp[j] <- (min(abs(segmentdata$values[j]-expected),0.5)*(1+abs(ploidy-2))^penploidy/(fraction[i-4]^penalty))^2}
+        else if(method=='SMRE') {temp[j] <- sqrt(min(abs(segmentdata$values[j]-expected),0.5)*(1+abs(ploidy-2))^penploidy/(fraction[i-4]^penalty))}
+        else if(method=='MAE') {temp[j] <- min(abs(segmentdata$values[j]-expected),0.5)*(1+abs(ploidy-2))^penploidy/(fraction[i-4]^penalty)}
         else {print("Not a valid method")}
       }
       if(method=='RMSE') {errorlist[i-4] <- sqrt(sum(rep(temp,segmentdata$lengths))/sum(segmentdata$lengths))}
@@ -507,8 +513,10 @@ squaremodel <- function(template, QDNAseqobjectsample = FALSE, prows=100, ptop=5
   minimat <- matrix(nrow=(prows+1),ncol=96)
   for (i in 1:(prows+1)) {
     for (j in 1:96) {
-      if (i==1|i==(prows+1)|j==1|j==96) {
+      if (i==1|i==(prows+1)) {
         minimat[i,j] <- FALSE
+      } else if (j==96) {
+        minimat[i,j] <- errormatrix[i,j]==min(errormatrix[(i-1):(i+1),(j-1):j])
       } else {
         minimat[i,j] <- errormatrix[i,j]==min(errormatrix[(i-1):(i+1),(j-1):(j+1)])
       }
@@ -635,13 +643,15 @@ singleplot <- function(template,cellularity = 1, error, ploidy = 2, standard, ti
 # similarly to singleplot, this function takes a model and its template dataframe
 # it returns a new dataframe with information about the individual segments: genomic location (chromosome, start, end),
 # the segment value and the corresponding standard error and the p-value (in log10) associated with the nearest absolute copy number
-getadjustedsegments <- function(template,cellularity = 1, ploidy = 2, standard = 1, QDNAseqobjectsample = FALSE, log=FALSE) {
+getadjustedsegments <- function(template,cellularity = 1, ploidy = 2, standard, QDNAseqobjectsample = FALSE, log=FALSE) {
   if(QDNAseqobjectsample) {template <- ObjectsampleToTemplate(template, QDNAseqobjectsample)}
+  segmentdata <- rle(as.vector(na.exclude(template$segments)))
+  if(missing(standard)) { standard <- median(rep(segmentdata$values,segmentdata$lengths)) }
   adjustedcopynumbers <- ploidy + (template$copynumbers-standard)/((1/ploidy)*cellularity*standard)
   adjustedsegments <- ploidy + (template$segments-standard)/((1/ploidy)*cellularity*standard)
   template.na <- na.exclude(template)
-  segmentdata <- rle(as.vector(na.exclude(adjustedsegments)))
-  copynumberdata <- as.vector(na.exclude(adjustedcopynumbers))
+  adjsegmentdata <- rle(as.vector(na.exclude(adjustedsegments)))
+  adjcopynumberdata <- as.vector(na.exclude(adjustedcopynumbers))
   Chromosome <- c()
   Start <- c()
   End <- c()
@@ -652,20 +662,20 @@ getadjustedsegments <- function(template,cellularity = 1, ploidy = 2, standard =
   Copies <- c()
   P_log10 <- c()
   counter <- 1
-  for (i in 1:length(segmentdata$lengths)) {
+  for (i in 1:length(adjsegmentdata$lengths)) {
     Chromosome[i] <- as.vector(template.na$chr)[counter]
     Start[i] <- as.vector(template.na$start)[counter]
-    End[i] <- as.vector(template.na$end)[counter+segmentdata$lengths[i]-1]
-    Num_Probes[i] <- segmentdata$lengths[i]
-    if(log==TRUE) {Segment_Mean[i] <- log2(segmentdata$values[i])}
+    End[i] <- as.vector(template.na$end)[counter+adjsegmentdata$lengths[i]-1]
+    Num_Probes[i] <- adjsegmentdata$lengths[i]
+    if(log==TRUE) {Segment_Mean[i] <- log2(adjsegmentdata$values[i])}
     if(log==FALSE) {
-      Segment_Mean[i] <- segmentdata$values[i]
-      Segment_Mean2[i] <- mean(copynumberdata[counter:(counter+segmentdata$lengths[i]-1)])
-      Segment_SE[i] <- sd(copynumberdata[counter:(counter+segmentdata$lengths[i]-1)])/sqrt(Num_Probes[i])
+      Segment_Mean[i] <- adjsegmentdata$values[i]
+      Segment_Mean2[i] <- mean(adjcopynumberdata[counter:(counter+adjsegmentdata$lengths[i]-1)])
+      Segment_SE[i] <- sd(adjcopynumberdata[counter:(counter+adjsegmentdata$lengths[i]-1)])/sqrt(Num_Probes[i])
       Copies[i] <- round(Segment_Mean2[i])
       P_log10[i] <- round(log(2*(1-pnorm(abs(Copies[i]-Segment_Mean2[i])/Segment_SE[i])),10),digits=3)
     }
-    counter <- counter+segmentdata$lengths[i]
+    counter <- counter+adjsegmentdata$lengths[i]
   }
   if(log==TRUE) {df <- data.frame(Chromosome,Start,End,Num_Probes,Segment_Mean)}
   if(log==FALSE) {df <- data.frame(Chromosome,Start,End,Num_Bins=Num_Probes,Segment_Mean,Segment_Mean2,Segment_SE,Copies,P_log10)}
